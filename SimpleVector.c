fd460c9e2da9493702fe49116a0beab088016885
Abdelrahman Megahed <abdelrahmanmegahed@Abdelrahmans-MacBook-Pro.local>
2017-10-23 12:55:05 -0400

    Delete SiSimple Vector files

--- SimpleVector.c
+++
@@ -1,9 +0,0 @@
-//
-//  SimpleVector.c
-//  Product123
-//
-//  Created by Abdelrahman Megahed on 2017-10-07.
-//  Copyright © 2017 Abdelrahman Megahed. All rights reserved.
-//
-
-#include "SimpleVector.h"
